27983479a85881d40e877d40b769a94df2971748
Samara Akim <Samara@TobiasArztAir6.fritz.box>
2024-12-01 21:47:53 +0100

    Add AppTest, pom.xml, and CI/CD workflow

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,16 @@
+package com.example;
+
+public class App {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Java CI/CD Demo!");
+    }
+}
+
---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+    @Test
+    public void testAdd() {
+        App app = new App();
+        assertEquals(5, app.add(2, 3));
+    }
+
+    @Test
+    public void testSubtract() {
+        App app = new App();
+        assertEquals(2, app.subtract(5, 3));
+    }
+}
+
